c60f3c6251983fb5a69077fe868ac7e8d3abf22b
Yezen Armout <58712447+yarmout@users.noreply.github.com>
2023-04-25 23:02:52 -0400

    Add content to Projects section

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+        title: "Project 1",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: require("./images/puppy.jpeg"),
+        link: "https://google.com",
+    },
+    {
+        title: "Project 2",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: require("./images/puppy.jpeg"),
+        link: "https://google.com",
+    },
+    {
+        title: "Project 3",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: require("./images/puppy.jpeg"),
+        link: "https://google.com",
+    },
+    {
+        title: "Project 4",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: require("./images/puppy.jpeg"),
+        link: "https://google.com",
+    },
+];
